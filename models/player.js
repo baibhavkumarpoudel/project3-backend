8b8d704f4b846c8c88516f65bc235bd1e1991887
Baibhav K Poudel <bkpoudel@up.com>
2021-06-15 14:29:05 -0500

    got the associations done

--- models/player.js
+++ models/player.js
@@ -1,25 +1,12 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Player extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Player.init({
+  const Player = sequelize.define('Player', {
     name: DataTypes.STRING,
     age: DataTypes.STRING,
     position: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Player',
-  });
+  }, {});
+  Player.associate = function(models) {
+    Player.belongsTo(models.Team, { foreignKey: 'teamId' })
+  };
   return Player;
 };
--- models/team.js
+++ models/team.js
@@ -1,27 +1,14 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Team extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Team.init({
+  const Team = sequelize.define('Team', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
     name: DataTypes.STRING,
     division: DataTypes.STRING,
     img: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Team',
-  });
+  }, {});
+  Team.associate = function(models) {
+    Team.hasMany(models.Player, { foreignKey: 'teamId' })
+  };
   return Team;
 };
