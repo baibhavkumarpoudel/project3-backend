858d072992d18ff11e6f2d180b616d874c9ac053
Baibhav K Poudel <bkpoudel@up.com>
2021-06-16 12:26:26 -0500

    worked on controller files

--- controllers/player.js
+++ controllers/player.js
@@ -1,76 +1,28 @@
 const Player = require('../models').Player;
-const constants = require('../constants');
 
-const getPlayerById = (req, res) => {
-
-    Player.findByPk(req.params.player, {
-        include: [
-            {
-                model: Player,
-                attributes: ['id', 'name', 'age', 'position', 'teamId'],
-            }
-        ],
- 
-    })
-    .then(foundPlayer => {
-        if(foundPlayer === null){
-            res.status(constants.BAD_REQUEST).send('ERROR: Incorrect Player Id')
-        }else{
-            res.status(constants.SUCCESS).json(foundPlayer)
-        }
-    })
-    .catch(err => {
-        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+const getPlayerById = (req,res) => {
+    Player.findByPk(req.params.index).then(player=> {
+        res.render('show.ejs', {player: player})
     })
 }
 
-const editPlayer = (req, res) => {
+const editPlayer = (req,res) => {
+
     Player.update(req.body, {
-        where: {
-            id: req.params.playerId
-        },
-        returning: true
-    })
-    .then(updatedPlayer => {
-        if(updatedPlayer[0] === 0) {
-            res.status(constants.BAD_REQUEST).send('ERROR: Incorrect Post Id')
-        }else{
-            Player.findByPk(req.params.playerId, {
-                include: [
-                    {
-                        model: Player,
-                        attributes: ['id', 'name', 'age', 'position', 'teamId'],
-                    }
-                ]
-            })
-            .then(foundPlayer => {
-                if(foundPlayer === null){
-                    res.status(constants.BAD_REQUEST).send('ERROR: Incorrect Player Id')
-                }else{
-                    res.status(constants.SUCCESS).json(foundPlayer)
-                }
-            })
-            .catch(err => {
-                res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
-            })
-        }
+        where: {id: req.params.index },
+        returning: true 
+    }).then(player => {
+            res.redirect('/player' + req.params.index)
     })
 }
 
-const deletePlayer = (req, res) => {
-    Player.destroy({
-        where: {
-            id: req.params.playerId
-        }
-    })
-    .then(deletedPlayer => {
-        console.log("Player Deleted")
-        res.status(constants.SUCCESS).send('success')
-    })
-    .catch(err => {
-        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+const deletePlayer = (req,res) => {
+    Player.destroy({ where : { id: req.params.index}}).then(() => {
+        res.redirect('/player')
     })
 }
+
+
 
 module.exports = {
     getPlayerById,
--- controllers/team.js
+++ controllers/team.js
@@ -2,50 +2,20 @@ const Team = require('../models').Team;
 const Player = require('../models').Player;
 
 
-const constants = require('../constants');
-
-const getProfile = (req, res) => {
-    
-    Team.findByPk(req.user.id, {
-        include: [
-            {
-                model: Player,
-                attributes: ['id', 'name', 'age', 'position', 'teamId']
-            }
-        ],
-        attributes: ['id', 'username', 'password', 'name', 'division', 'img'],
-    })
-    .then(userProfile => {
-        res.status(constants.SUCCESS).json(userProfile)
-    })
-    .catch(err => {
-        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
-    })
+const getProfile = (req,res) => {
+    Team.findByPk(req.params.index, {
+        include: [Player]
+    }).then(team => {
+        res.render('team/profile.ejs', { team:team})
+        })
 }
 
-const editProfile = (req, res) => {
+const editProfile = (req,res) => {
     Team.update(req.body, {
-        where: {
-            id: req.user.id
-        },
+        where: { id: req.params.index },
         returning: true
-    })
-    .then(() => {
-        Team.findByPk(req.user.id, {
-            include: [
-                {
-                    model: Player,
-                    attributes: ['id', 'name', 'age', 'position', 'teamId']
-                }
-            ],
-            attributes: ['id', 'username', 'password', 'name', 'division', 'img'],
-        })
-        .then(userProfile => {
-            res.status(constants.SUCCESS).json(userProfile)
-        })
-    })
-    .catch(err => {
-        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    }).then(team => {
+        res.redirect('/profile'+req.params.index);
     })
 }
 
@@ -53,3 +23,4 @@ module.exports = {
     getProfile,
     editProfile
 }
+
